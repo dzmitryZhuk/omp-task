b35e98abe9531139a4fddfc9361b6a583b75f7e0
dzmitryZhuk <dzmitry.zhuk@bk.ru>
2024-02-28 15:18:07 +0300

    upd

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,12 +1,14 @@
 #include "MainWindow.h"
 
+#include <QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent)
   : QMainWindow(parent)
   , toolPanel_(new ToolPanel{this})
   , canvas_(new Canvas{this})
 {
-    addToolBar(Qt::TopToolBarArea, toolPanel_);
-    toolPanel_->setMovable(false);
+  addToolBar(Qt::TopToolBarArea, toolPanel_);
+  toolPanel_->setMovable(false);
 }
 
 MainWindow::~MainWindow()
