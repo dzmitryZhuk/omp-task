5696be4c325012c457d23fbc6b30f16fb4b8a38f
dzmitryZhuk <dzmitry.zhuk@bk.ru>
2024-02-28 21:23:28 +0300

    upd Figure: add some virtual methods

--- src/Figure.h
+++ src/Figure.h
@@ -1,11 +1,18 @@
 #pragma once
 
 #include <QObject>
+#include <QPainter>
+#include <QPointF>
 
-class Figure : public QObject
+class Figure
+  : public QObject
 {
   Q_OBJECT
 public:
   explicit Figure(QObject *parent = nullptr);
   virtual ~Figure();
+
+  virtual void draw(QPainter *painter) const = 0;
+  virtual void move(double dx, double dy) const = 0;
+  virtual void contains(const QPointF &point) const = 0;
 };
