003b647048aff9f4328d18a30d272cc1f9e86948
dzmitryZhuk <dzmitry.zhuk@bk.ru>
2024-02-28 23:40:07 +0300

    upd MainWindow: add canvas

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -7,8 +7,12 @@ MainWindow::MainWindow(QWidget *parent)
   , toolPanel_(new ToolPanel{this})
   , canvas_(new Canvas{this})
 {
+  // tool panel initialization
   addToolBar(Qt::TopToolBarArea, toolPanel_);
   toolPanel_->setMovable(false);
+
+  // canvas initialization
+  setCentralWidget(canvas_);
 }
 
 MainWindow::~MainWindow()
