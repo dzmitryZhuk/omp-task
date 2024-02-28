81cc733b3c167534cf34875f4fd15991a9e08f39
dzmitryZhuk <dzmitry.zhuk@bk.ru>
2024-02-28 14:00:54 +0300

    upd

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
-int main(int argc, char argv[])
+#include "MainWindow.h"
+
+#include <QApplication>
+
+int main(int argc, char* argv[])
 {
-    return 0;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
