90ddbec54dd87ebddc37b81d3aa94baf152dd186
dzmitryZhuk <dzmitry.zhuk@bk.ru>
2024-03-01 02:07:00 +0300

    add to Figure member boundingRect

--- src/Figures/Figure.cpp
+++ src/Figures/Figure.cpp
@@ -1,7 +1,13 @@
 #include "Figure.h"
 
 Figure::Figure(QObject *parent)
-    : QObject(parent)
+  : QObject(parent)
+{
+}
+
+Figure::Figure(const QPointF &first, const QPointF &second, QObject *parent)
+  : QObject(parent)
+  , boundingRect_(QRectF{first, second})
 {
 }
 
--- src/Figures/Figure.h
+++ src/Figures/Figure.h
@@ -3,6 +3,7 @@
 #include <QObject>
 #include <QPainter>
 #include <QPointF>
+#include <QRectF>
 
 class Figure
   : public QObject
@@ -10,9 +11,13 @@ class Figure
   Q_OBJECT
 public:
   explicit Figure(QObject *parent = nullptr);
+  Figure(const QPointF &first, const QPointF &second, QObject *parent = nullptr);
   virtual ~Figure();
 
   virtual void draw(QPainter *painter) = 0;
   virtual void move(double dx, double dy) = 0;
   virtual bool contains(const QPointF &point) const = 0;
+  virtual void setSecondPoint(const QPointF &point) = 0;
+protected:
+  QRectF boundingRect_;
 };
