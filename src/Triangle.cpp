e06460e49851eb23d1305f1b03e69d152f9de5ac
dzmitryZhuk <dzmitry.zhuk@bk.ru>
2024-02-29 21:13:47 +0300

    realize virtual methods in class Triangle

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -5,6 +5,39 @@ Triangle::Triangle(QObject *parent)
 {
 }
 
+Triangle::Triangle(const QPointF &p1, const QPointF &p2, const QPointF &p3, QObject *parent)
+  : Figure{parent}
+  , p1_(p1)
+  , p2_(p2)
+  , p3_(p3)
+{
+}
+
 Triangle::~Triangle()
 {
+}
+
+void Triangle::draw(QPainter *painter)
+{
+  QPolygonF triangle;
+  triangle << p1_ << p2_ << p3_;
+  painter->drawPolygon(triangle);
+}
+
+void Triangle::move(double dx, double dy)
+{
+  p1_.rx() += dx;
+  p1_.ry() += dy;
+  p2_.rx() += dx;
+  p2_.ry() += dy;
+  p3_.rx() += dx;
+  p3_.ry() += dy;
+  // draw
+}
+
+bool Triangle::contains(const QPointF &point) const
+{
+  QPolygonF polygon;
+  polygon << p1_ << p2_ << p3_;
+  return polygon.containsPoint(point, Qt::OddEvenFill);
 }
--- src/Triangle.h
+++ src/Triangle.h
@@ -8,10 +8,15 @@ class Triangle
   : public Figure
 {
 public:
-    explicit Triangle(QObject *parent = nullptr);
-    virtual ~Triangle();
+  explicit Triangle(QObject *parent = nullptr);
+  Triangle(const QPointF &p1, const QPointF &p2, const QPointF &p3, QObject *parent = nullptr);
+  virtual ~Triangle();
+
+  virtual void draw(QPainter *painter) override;
+  virtual void move(double dx, double dy) override;
+  virtual bool contains(const QPointF &point) const override;
 private:
-    QPointF p1_;
-    QPointF p2_;
-    QPointF p3_;
+  QPointF p1_;
+  QPointF p2_;
+  QPointF p3_;
 };
