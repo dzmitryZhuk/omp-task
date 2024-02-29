6ad9e759eb643a8257419cba44491511484b713a
dzmitryZhuk <dzmitry.zhuk@bk.ru>
2024-02-29 21:38:32 +0300

    reqlize virtual methods in class Ellipse

--- src/Figures/Ellipse.cpp
+++ src/Figures/Ellipse.cpp
@@ -5,6 +5,38 @@ Ellipse::Ellipse(QObject *parent)
 {
 }
 
+Ellipse::Ellipse(const QRectF &rect, QObject *parent)
+  : Figure{parent}
+  , rect_(rect)
+{
+}
+
 Ellipse::~Ellipse()
 {
+}
+
+void Ellipse::draw(QPainter *painter)
+{
+  painter->drawEllipse(rect_);
+}
+
+void Ellipse::move(double dx, double dy)
+{
+  rect_.translate(dx, dy);
+  // draw
+}
+
+bool Ellipse::contains(const QPointF &point) const
+{
+  if (rect_.contains(point))
+  {
+    auto x = point.x();
+    auto y = point.y();
+    auto x0 = rect_.center().x();
+    auto y0 = rect_.center().y();
+    auto a = rect_.width() / 2.0;
+    auto b = rect_.height() / 2.0;
+    return (((x - x0) * (x - x0)) / (a * a) + ((y - y0) * (y - y0)) / (b * b)) <= 1;
+  }
+  return false;
 }
--- src/Figures/Ellipse.h
+++ src/Figures/Ellipse.h
@@ -7,6 +7,13 @@
 class Ellipse : public Figure
 {
 public:
-    explicit Ellipse(QObject *parent = nullptr);
-    virtual ~Ellipse();
+  explicit Ellipse(QObject *parent = nullptr);
+  Ellipse(const QRectF &rect, QObject *parent = nullptr);
+  virtual ~Ellipse();
+
+  virtual void draw(QPainter *painter) override;
+  virtual void move(double dx, double dy) override;
+  virtual bool contains(const QPointF &point) const override;
+private:
+  QRectF rect_;
 };
