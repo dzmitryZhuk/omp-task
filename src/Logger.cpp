484d9e9cb0f36c7074965f6ba165641bf12ae7b8
dzmitryZhuk <dzmitry.zhuk@bk.ru>
2024-03-02 16:39:42 +0300

    upd class Logger: add logging QString messages

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -7,7 +7,12 @@ void Logger::log(const char *message)
   Logger::log(std::string{message});
 }
 
-void Logger::log(std::string message)
+void Logger::log(const std::string &message)
 {
   std::cout << message << std::endl;
+}
+
+void Logger::log(const QString &message)
+{
+    Logger::log(message.toStdString());
 }
--- src/Logger.h
+++ src/Logger.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <string>
+#include <QString>
 
 class Logger
 {
 public:
   static void log(const char * message);
-  static void log(std::string message);
+  static void log(const std::string &message);
+  static void log(const QString &message);
 };
