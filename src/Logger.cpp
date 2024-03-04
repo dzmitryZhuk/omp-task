9c0d84b7164d5fa8f9c21ead2cfd78dc44fd1efa
dzmitryZhuk <dzmitry.zhuk@bk.ru>
2024-03-04 20:56:33 +0300

    upd Logger: log to file also

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,15 +1,28 @@
 #include "Logger.h"
 
 #include <iostream>
+#include <QFile>
+
+void Logger::log(const std::string &message)
+{
+#ifdef LOG
+  std::cout << message << std::endl;
+#ifdef LOG_TO_FILE
+  QString filename = "log.txt";
+#ifdef LOG_FILENAME
+  filename = LOG_FILENAME;
+#endif
+  QFile file{filename};
+  file.open(QFile::WriteOnly | QFile::Append);
+  file.write(message.c_str(), message.size());
+  file.write("\n");
+#endif
+#endif
+}
 
 void Logger::log(const char *message)
 {
   Logger::log(std::string{message});
-}
-
-void Logger::log(const std::string &message)
-{
-  std::cout << message << std::endl;
 }
 
 void Logger::log(const QString &message)
