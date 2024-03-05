8ec31f0402cd432f5a8d205054b60c58cae535ea
dzmitryZhuk <dzmitry.zhuk@bk.ru>
2024-03-05 17:34:39 +0300

    upd Logger: add timestamp in begin of log message

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -2,11 +2,16 @@
 
 #include <iostream>
 #include <QFile>
+#include <QDateTime>
 
 void Logger::log(const std::string &message)
 {
 #ifdef LOG
-  std::cout << message << std::endl;
+  auto current = QDateTime::currentDateTime();
+  std::string messageWithTimestamp = "[";
+  messageWithTimestamp += current.toString("yyyy.MM.dd hh:mm:ss.zzz").toStdString();
+  messageWithTimestamp += "] " + message;
+  std::cout << messageWithTimestamp << std::endl;
 #ifdef LOG_TO_FILE
   QString filename = "log.txt";
 #ifdef LOG_FILENAME
@@ -14,7 +19,7 @@ void Logger::log(const std::string &message)
 #endif
   QFile file{filename};
   file.open(QFile::WriteOnly | QFile::Append);
-  file.write(message.c_str(), message.size());
+  file.write(messageWithTimestamp.c_str(), messageWithTimestamp.size());
   file.write("\n");
 #endif
 #endif
